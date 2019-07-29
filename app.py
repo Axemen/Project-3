282606b6d477886d421d24c3f6c6568df16cccd0
Devynn Moreno <devynn.moreno@gmail.com>
2019-07-28 20:49:21 -0500

    initial heroku commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, render_template, send_file
+
+app = Flask(__name__)
+
+# render out an index page
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
